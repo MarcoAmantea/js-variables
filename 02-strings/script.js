a6997c15e63fdcb1b765f63ae5b54dae0c1257c7
MarcoAmantea <marcoamantea18@gmail.com>
2024-10-25 17:06:03 +0200

    refactor: "name" not sbarred

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,8 +1,8 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
-const name = "Marco";
-console.log(name)
+const userName = "Marco";
+console.log(userName)
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
